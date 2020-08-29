f8ac1ba3ca84f315242c6d6fc2a139bb13c299de
Tony <jinnit28201@gmail.com>
2020-08-29 17:07:41 +0800

    新增多訊息框問答

--- BOTCH/Controllers/LineWebHookSampleController.cs
+++ BOTCH/Controllers/LineWebHookSampleController.cs
@@ -45,7 +45,42 @@ namespace BOTCH.Controllers
                             string RetuenMessage = MessageCommand.RPGCommand(LineEvent.message.text);
                             this.ReplyMessage(LineEvent.replyToken, RetuenMessage);
                         }
-                        else if(LineEvent.message.text.Contains("圖片問答-"))
+                        else if (LineEvent.message.text.Contains("多圖片問答-"))
+                        {
+                            isRock.LineBot.Bot bot;
+                            bot = new isRock.LineBot.Bot(channelAccessToken);
+                            //取得 http Post RawData(should be JSO
+                            string postData = Request.Content.ReadAsStringAsync().Result;
+                            var ReceivedMessage = isRock.LineBot.Utility.Parsing(postData);
+
+                            //建立actions，作為ButtonTemplate的用戶回覆行為
+                            var actions = new List<isRock.LineBot.TemplateActionBase>();
+                            actions.Add(new isRock.LineBot.MessageActon() { label = "標題-文字回覆", text = "回覆文字" });
+                            actions.Add(new isRock.LineBot.UriActon() { label = "標題-Google", uri = new Uri("http://www.google.com") });
+                            actions.Add(new isRock.LineBot.PostbackActon() { label = "標題-發生postack", data = "abc=aaa&def=111" });
+
+                            //單一Column 
+                            var Column = new isRock.LineBot.Column
+                            {
+                                text = "ButtonsTemplate文字訊息",
+                                title = "ButtonsTemplate標題",
+                                //設定圖片
+                                thumbnailImageUrl = new Uri("https://arock.blob.core.windows.net/blogdata201706/22-124357-ad3c87d6-b9cc-488a-8150-1c2fe642d237.png"),
+                                actions = actions //設定回覆動作
+                            };
+
+                            //建立CarouselTemplate
+                            var CarouselTemplate = new isRock.LineBot.CarouselTemplate();
+
+                            //這是範例，所以用一組樣板建立三個
+                            CarouselTemplate.columns.Add(Column);
+                            CarouselTemplate.columns.Add(Column);
+                            CarouselTemplate.columns.Add(Column);
+
+                            this.ReplyMessage(LineEvent.replyToken, new isRock.LineBot.TemplateMessage(CarouselTemplate));
+
+                        }
+                        else if (LineEvent.message.text.Contains("圖片問答-"))
                         {
                             isRock.LineBot.Bot bot;
                             bot = new isRock.LineBot.Bot(channelAccessToken);
